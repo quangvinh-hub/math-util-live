0dc200f3d6f14d77265930bc9713c807d775e254
quangvinh-hub <vinhdeptraivt@gmail.com>
2024-09-12 12:17:57 +0700

    Lan thu hai dua code len server

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -25,6 +25,10 @@ public class MathUtilLive {
         
         // MathUtil.getFactorial(-5); // ném ngoại lệ 
         
+        // Thêm code sau lần đầu tiên đưa code lên server 12:15pm 12/09/2024
+        System.out.println("expected: 1! = 1;   actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6;   actual: " + MathUtil.getFactorial(3));
+
         // kĩ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì 
         // expected value: 5! = 120
         // mày/hàm ai đó viết khi chạy thực tế là mấy - actual, giả sử 120 
